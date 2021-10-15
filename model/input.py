9a76508e42fb3ec281e89835be65470d06c062de
Mr-Lee007 <825339699@qq.com>
2021-10-15 21:31:21 +0800

    problem01(....the 1st assignment)

--- model/input.py
+++ model/input.py
@@ -4,7 +4,7 @@ import numpy as np
 import datetime
 
 class Employee():
-    def __init__(self, EmpNo, Captain, FirstOfficer, Deadhead, Base, DutyCostPerHour, ParingCostPerHour):
+    def __init__(self, EmpNo, Captain, FirstOfficer, Deadhead, Base, DutyCostPerHour, ParingCostPerHour, CapType, ArrvTime=None, ArrvStn=None):
         self.EmpNo = EmpNo
         self.Captain = Captain
         self.FirstOfficer = FirstOfficer
@@ -12,13 +12,17 @@ class Employee():
         self.Base = Base
         self.DutyCostPerHour = DutyCostPerHour
         self.ParingCostPerHour = ParingCostPerHour
+        self.CapType = CapType
+        self.ArrvTime = ArrvTime
+        self.ArrvStn = ArrvStn
 
     def __str__(self):
         return "[" + self.EmpNo + "," + str(self.Captain) + "," + str(self.FirstOfficer) + "," + str(
-            self.Deadhead) + "," + self.Base + "," + self.DutyCostPerHour + "," + self.ParingCostPerHour + "]"
+            self.Deadhead) + "," + self.Base + "," + self.DutyCostPerHour + "," + self.ParingCostPerHour + "," + str(self.CapType) + "]"
 
 class Flight():
-    def __init__(self, FltNum, DptrDate, DptrTime, DptrStn, ArrvDate, ArrvTime, ArrvStn,Comp, CompCaptain,CompFirstOfficer, Timestamp):
+    def __init__(self, Num, FltNum, DptrDate, DptrTime, DptrStn, ArrvDate, ArrvTime, ArrvStn,Comp, CompCaptain,CompFirstOfficer, DptrTimestamp ,ArrvTimestamp):
+        self.Num = Num
         self.FltNum = FltNum
         self.DptrDate = DptrDate
         self.DptrTime = DptrTime
@@ -29,11 +33,13 @@ class Flight():
         self.Comp = Comp
         self.CompCaptain = CompCaptain
         self.CompFirstOfficer = CompFirstOfficer
-        self.Timestamp = Timestamp
+        self.DptrTimestamp = DptrTimestamp
+        self.ArrvTimestamp = ArrvTimestamp
 
     def __str__(self):
-        return "[" + self.FltNum + "," + str(self.DptrDate) + "," + str(self.DptrTime) + "," + str(
-            self.DptrStn) + "," + str(self.DptrStn) + "," + str(self.ArrvDate) + "," + str(self.ArrvTime) + "," + str(self.Comp) + "," + str(self.Timestamp)+ "]"
+        return "[" + str(self.Num) + "," + self.FltNum + "," + str(self.DptrDate) + "," + str(self.DptrTime) + "," + str(
+            self.DptrStn) + "," + str(self.DptrStn) + "," + str(self.ArrvDate) + "," + str(self.ArrvTime) + "," + str(self.Comp) + "," + str(self.DptrTimestamp) + "," + str(self.ArrvTimestamp)+ "]"
+
 
 
 #读取Employee数据，并且提供一个建立Employee类的函数
@@ -51,8 +57,14 @@ def Read_crew(DataName, DataDir="../data/"):
         for row in data:
             row[1] = 1 if row[1] == 'Y' else 0
             row[2] = 1 if row[2] == 'Y' else 0
+            if row[1] == 1 and row[2] == 0:
+                CapType = 0
+            elif row[1] == 0 and row[2] == 1:
+                CapType = 1
+            else:
+                CapType = 2
             tmp = Employee(EmpNo=row[0], Captain=row[1], FirstOfficer=row[2], Deadhead=1, Base=row[4],
-                           DutyCostPerHour=row[5], ParingCostPerHour=row[6])
+                           DutyCostPerHour=row[5], ParingCostPerHour=row[6],CapType=CapType)
             emp.append(tmp)
         # for i in emp:
         #     print(i.EmpNo)
@@ -65,7 +77,7 @@ def Read_flight(DataName, DataDir="../data/"):
         header_row = next(data)
         # print(header_row)
         fli = []
-
+        num = 0
         for row in data:
             # row[1] = 1 if row[1] == 'Y' else 0
             if row[7] =='C1F1':
@@ -74,12 +86,15 @@ def Read_flight(DataName, DataDir="../data/"):
             elif row[7] == "C1F2":
                 CompCaptain = 1
                 CompFirstOfficer = 2
-            combine_time = row[1] + ' ' + row[2]  #将日期和时间组合在一起
+            Dptr_time = row[1] + ' ' + row[2]  #将日期和时间组合在一起
+            Arrv_time = row[4] + ' ' + row[5]  # 将日期和时间组合在一起
             row[1] = datetime.datetime.strptime(row[1], "%m/%d/%Y")
-            combine_time = datetime.datetime.strptime(combine_time, "%m/%d/%Y %H:%M")
-            tmp = Flight(FltNum=row[0], DptrDate=row[1], DptrTime=row[2], DptrStn=1, ArrvDate=row[4],
-                         ArrvTime=row[5], ArrvStn=row[6], Comp=row[7], CompCaptain=CompCaptain, CompFirstOfficer=CompFirstOfficer, Timestamp=combine_time)
+            Dptr_time = datetime.datetime.strptime(Dptr_time, "%m/%d/%Y %H:%M")
+            Arrv_time = datetime.datetime.strptime(Arrv_time, "%m/%d/%Y %H:%M")
+            tmp = Flight(Num=num,FltNum=row[0], DptrDate=row[1], DptrTime=row[2], DptrStn=row[3], ArrvDate=row[4],
+                         ArrvTime=row[5], ArrvStn=row[6], Comp=row[7], CompCaptain=CompCaptain, CompFirstOfficer=CompFirstOfficer, ArrvTimestamp=Arrv_time,DptrTimestamp=Dptr_time)
             fli.append(tmp)
+            num = num +1
         return fli
 
 
@@ -88,6 +103,6 @@ if __name__ == '__main__' :
     emp = Read_crew('Data A-Crew.csv')
     #costtime = fli[0].DptrTime - fli[0].ArrvTime
     #print(costtime)
-    print("origin:", fli[1].Timestamp)
+    print("origin:", emp[1].ArrvStn)
 
 
--- model/problem01.py
+++ model/problem01.py
@@ -1,41 +1,135 @@
 import input
 import datetime
+import argparse
 
-#1.读取数据
-emp = input.Read_crew(DataName='Data A-Crew.csv')
-fli = input.Read_flight(DataName='Data A-Flight.csv')
-print(fli)
 
-#2.先进行排序
-data_start = datetime.datetime.strptime("2021-08-11","%Y-%m-%d")
-data_end = datetime.datetime.strptime("2021-08-25","%Y-%m-%d")
-#sortedFlight = sorted(fli,key=lambda x: fli.DptrDate,reverse=True)  #ranking
-#print(sortedFlight)
-data = []
-data_rank = [data_start]
-delta = (data_end-data_start).days
-#生成一个日期排序
-for day in range(1,delta+1):
-    data_rank.append(data_start+datetime.timedelta(days=day))
-    data.append("day"+str(day))
-print(data)
+args = None
 
-#data_rank = range(data_end-data_start)
-#3.提取每日航班
-# for i in range(1,delta+1):
-#     for flight in fli:
-#         if flight.DptrDate == data_rank[i] :
-#             data.append(flight)
-#
-#
-# for j in range(1,delta+1):
-#     print(data[j])
-day =0
-for i in data_rank:
+def parse_args() :
+    parser = argparse.ArgumentParser()
+
+    # data parameters
+    parser.add_argument('--data_crew_name', type=str, default='Data A-Crew.csv', help='the name of the crew data')
+    parser.add_argument('--data_flight_name', type=str, default='Data A-Flight.csv', help='the name of the flight data')
+    parser.add_argument('--data_dir', type=str, default='', help='the directory of the data')
+
+    # input parameters
+    parser.add_argument('--MinCT', type=int, default=40, help='the minimum connected time')
+    parser.add_argument('--MaxBLk', type=int, default=600, help='the maximum of the flight time during a duty')
+    parser.add_argument('--MaxDP', type=int, default=720, help='the maximum of the time during a duty')
+    parser.add_argument('--MinRest', type=int, default=660, help='the minimum of the rest time between two adjacent duty')
+    parser.add_argument('--MaxDH', type=int, default=5, help='the maximum of the people deadheading')
+    parser.add_argument('--MaxTAFB', type=int, default=14400, help='the maximum of the total time of the pairing')
+    parser.add_argument('--MaxSuccOn', type=int, default=4, help='the maximum of the succeed on duty days')
+    parser.add_argument('--MinVacDay', type=int, default=2, help='the minimum vacation days between two adjacent pairings')
+
+    # model parameters
+    parser.add_argument('--kState', type=int, default=2, help='the state of the pilot participate in the flight')
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    #1.读取数据
+    emp = input.Read_crew(DataName=args.data_crew_name)
+    fli = input.Read_flight(DataName=args.data_flight_name)
+    print(fli)
+
+    #2.机长分组
+    emp_group = {}
+    for employee in emp:
+        #print("flight", flight,num)
+        type = employee.CapType
+        if type in emp_group:
+            emp_group[type].append(employee)
+        else:
+            emp_group[type] = []
+            emp_group[type].append(employee)
+
+    #3.先进行排序
+    data_start = datetime.datetime.strptime("2021-08-11","%Y-%m-%d")
+    data_end = datetime.datetime.strptime("2021-08-25","%Y-%m-%d")
+    #sortedFlight = sorted(fli,key=lambda x: fli.DptrDate,reverse=True)  #ranking
+    #print(sortedFlight)
+    data = []
+    data_rank = [data_start]
+    delta = (data_end-data_start).days
+    #生成一个日期排序
+    for day in range(1,delta+1):
+        data_rank.append(data_start+datetime.timedelta(days=day))
+    print(data_rank)
+
+
+    # 4.提取每日航班
+    flight_day = {}
     for flight in fli:
-        if flight.DptrDate == i :
-            data.append(flight)
-    day = day +1
+        # print("flight", flight,num)
+        flight_date = flight.DptrDate.date()
+        if flight_date in flight_day:
+            flight_day[flight_date].append(flight)
+        else:
+            flight_day[flight_date] = []
+            flight_day[flight_date].append(flight)
+    # day =0
+    # for i in data_rank:
+    #     for flight in fli:
+    #         if flight.DptrDate == i :
+    #             data.append(flight)
+    #     day = day +1
+    #
+    # for j in data:
+    #     print(j)
 
-for j in data:
-    print(j)
+
+    #5.安排飞行员
+    emp_arrange = {}
+    ArrvImf = {}
+    print("test:", flight_day[datetime.datetime.strptime("2021-8-11", "%Y-%m-%d").date()])
+    days = len(flight_day)
+    Capnum = 0
+    FistOfficernum = 0
+    num = 0
+
+    for i in data_rank:
+        print("coming:", flight_day[i.date()])
+        while (len(flight_day[i.date()])!=0):
+            FirstFlight = flight_day[i.date()][0]
+            #分配机长并且同时记录ArrvTime和ArrvStn（到达机场），这里需要加个判断机长是不是用完了？动用type = 3 全能机长
+            if emp_group[0][Capnum] in emp_arrange:
+                emp_arrange[emp_group[0][Capnum]].append(FirstFlight)
+                emp_group[0][Capnum].ArrvTime = FirstFlight.ArrvTime
+                emp_group[0][Capnum].ArrvStn = FirstFlight.ArrvStn
+            else:
+                emp_arrange[emp_group[0][Capnum]] = []
+                emp_arrange[emp_group[0][Capnum]].append(FirstFlight)
+                emp_group[0][Capnum].ArrvTime = FirstFlight.ArrvTime
+                emp_group[0][Capnum].ArrvStn = FirstFlight.ArrvStn
+            #分配一名副机长，同时记录ArrvTime和ArrvStn（到达机场）
+            if emp_group[1][FistOfficernum] in emp_arrange:
+                emp_arrange[emp_group[1][FistOfficernum]].append(FirstFlight)
+                emp_group[1][FistOfficernum].ArrvTime = FirstFlight.ArrvTime
+                emp_group[1][FistOfficernum].ArrvStn = FirstFlight.ArrvStn
+            else:
+                emp_arrange[emp_group[1][FistOfficernum]] = []
+                emp_arrange[emp_group[1][FistOfficernum]].append(FirstFlight)
+                emp_group[1][FistOfficernum].ArrvTime = FirstFlight.ArrvTime
+                emp_group[1][FistOfficernum].ArrvStn = FirstFlight.ArrvStn
+            #记录完信息，从列表李删除该航班信息
+            del flight_day[i.date()][0]
+            print("hello",emp_group[0][Capnum].ArrvStn)
+
+            #检测剩余航班有满足继续飞行的要求：
+            for flight in flight_day[i.date()]:
+                print((emp_group[0][Capnum].ArrvStn == flight.DptrStn))
+                time_error = (flight.DptrTimestamp - emp_group[0][Capnum].ArrvTime).seconds
+                print("时间差：", time_error)
+                # if (emp_group[0][Capnum].ArrvStn == flight.DptrStn) and (flight.DptrTimestamp - emp_group[0][Capnum].ArrvTime >= args.MinCT):  #满足进行飞行
+                #     print("find")
+                # else:
+                #     print("no")
+
+
+    #print("the 1st assignment:",emp_arrange)
