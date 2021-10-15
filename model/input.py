984c06a609e19c0121a8ab420e8b46d75feb240a
Mr-Lee007 <825339699@qq.com>
2021-10-15 09:56:23 +0800

    更新两个读取函数 Read_crew(DataName) Read_flight(DataName) 返回值是一个数组，调用时[i].EmpNo = A0000 (路径已经默认是在/data之下了)

--- model/input.py
+++ model/input.py
@@ -16,27 +16,37 @@ class Employee():
     def __str__(self):
         return "[" + self.EmpNo + "," + str(self.Captain) + "," + str(self.FirstOfficer) + "," + str(
             self.Deadhead) + "," + self.Base + "," + self.DutyCostPerHour + "," + self.ParingCostPerHour + "]"
-    # def catch_mice(self):
-    #     """抓老鼠的方法"""
-    #     print('抓老鼠')
-    #
-    # def eat_mice(self):
-    #     """吃老鼠"""
-    #     print('吃老鼠')
-        #
-        # def eat_mice(self):
-        #     """吃老鼠"""
-        #     print('吃老鼠')
+
+class Flight():
+    def __init__(self, FltNum, DptrDate, DptrTime, DptrStn, ArrvDate, ArrvTime, ArrvStn, Comp):
+        self.FltNum = FltNum
+        self.DptrDate = DptrDate
+        self.DptrTime = DptrTime
+        self.DptrStn = DptrStn
+        self.ArrvDate = ArrvDate
+        self.ArrvTime = ArrvTime
+        self.ArrvStn = ArrvStn
+        self.Comp = Comp
+
+    def __str__(self):
+        return "[" + self.FltNum + "," + str(self.DptrDate) + "," + str(self.DptrTime) + "," + str(
+            self.DptrStn) + "," + str(self.DptrStn) + "," + str(self.ArrvDate) + "," + str(self.ArrvTime) +"," + str(self.Comp) +"]"
+
 class cat():
     def __init__(self,EmpNo):
         self.EmpNo = EmpNo
 
-#提供一个建立Employee类的函数
-def Data_processing(DataName, DataDir):
+#读取Employee数据，并且提供一个建立Employee类的函数
+def Read_crew(DataName, DataDir="../data/"):
+    """
+    :param DataName: 文件名称，，需要带引号，eg. 'Data A-Flight.csv'
+    :param DataDir: 默认路径为这个
+    :return: emp 是一个list，调用时emp[i] 表示第i个人的信号，eg.  emp[10].EmpNo = A0000
+    """
     with open(DataDir+DataName) as f:
         data = csv.reader(f)
         header_row = next(data)
-        print(header_row)
+        #print(header_row)
         emp = []
         for row in data:
             row[1] = 1 if row[1] == 'Y' else 0
@@ -48,33 +58,25 @@ def Data_processing(DataName, DataDir):
         #     print(i.EmpNo)
         return emp
 
+#读取航班数据，并且建立一个Flight的类
+def Read_flight(DataName, DataDir="../data/"):
+    with open(DataDir+DataName) as f:
+        data = csv.reader(f)
+        header_row = next(data)
+        # print(header_row)
+        fli = []
+
+        for row in data:
+            # row[1] = 1 if row[1] == 'Y' else 0
+            # row[2] = 1 if row[2] == 'Y' else 0
+            tmp = Flight(FltNum=row[0], DptrDate=row[1], DptrTime=row[2], DptrStn=1, ArrvDate=row[4],
+                         ArrvTime=row[5], ArrvStn=row[6], Comp=row[7])
+            fli.append(tmp)
+        return fli
+
 if __name__ == '__main__' :
-    emp = Data_processing('Data A-Crew.csv', '../data/')
-    print(emp[1].EmpNo)
-    # with open("../data/Data A-Crew.csv") as f:
-    #     data = csv.reader(f)
-    #     header_row = next(data)
-    #     print(header_row)
-    #     emp = []
-    #
-    #     for row in data:
-    #         row[1] = 1 if row[1] == 'Y' else 0
-    #         row[2] = 1 if row[2] == 'Y' else 0
-    #         tmp = Employee(EmpNo=row[0], Captain=row[1], FirstOfficer=row[2], Deadhead=1, Base=row[4],
-    #                        DutyCostPerHour=row[5], ParingCostPerHour=row[6])
-    #         emp.append(tmp)
-    #     for i in emp:
-    #         print(i.EmpNo)
+    fli = Read_flight(DataName='Data A-Flight.csv')
+    emp = Read_crew('Data A-Crew.csv')
+    print(fli[0].DptrTime)
+    print(emp[1].Captain)
 
-
-
-    # data = open("../data/Data A-Crew.csv",'rb')
-    # data_mid = np.loadtxt(data)
-    # data.close()
-    # data_arr = np.array(data_mid)
-    #a = sizeof(data_list)
-    # print('daxiao',my_matrix)
-    # # for row in data_list:
-    # #     for col in row:
-    #
-    # print("test", my_matrix[0][0])
